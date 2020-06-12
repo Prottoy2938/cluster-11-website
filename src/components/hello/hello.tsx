5c672f3bc47ab11cebe83838acb081f984d2bb11
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-12 15:28:21 +0600

    This should fail the test

--- src/components/hello/hello.tsx
+++ src/components/hello/hello.tsx
@@ -5,7 +5,7 @@ import styles from "./hello.module.css";
 const Hello: React.FC<Props> = (props: Props) => {
   const { name } = props;
   const [state, setState] = useState(name);
-  const title = "Hello";
+  const title = "Hello!";
   return (
     <h1 data-testid="title" className={styles.h1}>
       {title} {state}
