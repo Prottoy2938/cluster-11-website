cee31c2b0e4e1301f228b4106a47e156ad67612f
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-16 11:38:04 +0600

    Changed background canvas - added a new canvas

--- src/components/bouncing-ball-canvas/bouncing-ball-canvas.tsx
+++ src/components/bouncing-ball-canvas/bouncing-ball-canvas.tsx
@@ -5,104 +5,233 @@ const BouncingBallCanvas: React.FC = (props) => {
   const canvas = useRef(null);
 
   useEffect(() => {
-    const ctx = canvas.current.getContext("2d");
+    (function () {
+      "use strict";
+      window.addEventListener("load", function () {
+        if (!canvas || !canvas.current.getContext) {
+          return false;
+        }
 
-    const width = (canvas.current.width = window.innerWidth);
-    const height = (canvas.current.height = window.innerHeight);
-    const balls = [];
+        /********************
+          Random Number
+        ********************/
 
-    function random(min: number, max: number): number {
-      const num = Math.floor(Math.random() * (max - min)) + min;
-      return num;
-    }
+        function rand(min, max): number {
+          return Math.floor(Math.random() * (max - min + 1) + min);
+        }
 
-    function Ball(x, y, velX, velY, color, size): void {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
-    }
+        /********************
+          const
+        ********************/
 
-    Ball.prototype.draw = function (): void {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-      ctx.fill();
-    };
+        // canvas
+        const ctx = canvas.current.getContext("2d");
+        let X = (canvas.current.width = window.innerWidth);
+        let Y = (canvas.current.height = window.innerHeight);
+        let mouseX = X / 2;
+        let mouseY = Y / 2;
+        let blooms = [];
+        const bloomNum = 100;
+        const jointNum = 8 + 2;
+        let minRadius = 100;
+        let maxRadius = 130;
+        const flg = true;
 
-    Ball.prototype.update = function (): void {
-      if (this.x + this.size >= width) {
-        this.velX = -this.velX;
-      }
+        if (X < 768) {
+          minRadius = 70;
+          maxRadius = 100;
+        }
 
-      if (this.x - this.size <= 0) {
-        this.velX = -this.velX;
-      }
+        /********************
+          Animation
+        ********************/
 
-      if (this.y + this.size >= height) {
-        this.velY = -this.velY;
-      }
-      if (this.y - this.size <= 0) {
-        this.velY = -this.velY;
-      }
-      this.x += this.velX;
-      this.y += this.velY;
-    };
+        if (window) {
+          window.requestAnimationFrame =
+            window.requestAnimationFrame || window.webkitRequestAnimationFrame;
+        }
 
-    Ball.prototype.collisionDetect = function (): void {
-      for (let j = 0; j < balls.length; j++) {
-        if (!(this === balls[j])) {
-          const dx = this.x - balls[j].x;
-          const dy = this.y - balls[j].y;
-          const distance = Math.sqrt(dx * dx + dy * dy);
+        /********************
+          Bloom
+        ********************/
 
-          if (distance < this.size + balls[j].size) {
-            balls[j].color = this.color =
-              "rgb(" +
-              random(0, 255) +
-              "," +
-              random(0, 255) +
-              "," +
-              random(0, 255) +
-              ")";
+        function Bloom(ctx, x, y, i): void {
+          this.ctx = ctx;
+          this.init(x, y, i);
+        }
+
+        Bloom.prototype.init = function (x, y, i): void {
+          this.x = x;
+          this.y = y;
+          this.i = i;
+          this.r = rand(2, 12);
+          this.a = 0;
+          this.rad = (this.a * Math.PI) / 180;
+          this.radius = rand(minRadius, maxRadius);
+          this.joints = this.getJoints();
+          this.c = {
+            r: rand(0, 255),
+            g: rand(0, 255),
+            b: rand(0, 255),
+          };
+        };
+
+        Bloom.prototype.getJoints = function () {
+          const joints = [];
+          for (let i = 1; i < jointNum; i++) {
+            const a = rand(0, 360);
+            const rad = (a * Math.PI) / 180;
+            joints.push(rad);
+          }
+          return joints;
+        };
+
+        Bloom.prototype.draw = function () {
+          const ctx = this.ctx;
+          ctx.save();
+          ctx.fillStyle =
+            "rgb(" + this.c.r + ", " + this.c.g + ", " + this.c.b + ")";
+          ctx.strokeStyle =
+            "rgb(" + this.c.r + ", " + this.c.g + ", " + this.c.b + ")";
+          ctx.globalCompositeOperation = "lighter";
+          let preX = this.x;
+          let preY = this.y;
+          for (let i = 1; i < jointNum; i++) {
+            const x = (Math.cos(this.joints[i] * i) * this.radius) / i + preX;
+            const y = (Math.sin(this.joints[i] * i) * this.radius) / i + preY;
+            ctx.beginPath();
+            ctx.arc(x, y, this.r, 0, Math.PI * 2, false);
+            ctx.fill();
+            if (flg === true && i !== 1) {
+              ctx.beginPath();
+              ctx.moveTo(preX, preY);
+              ctx.lineTo(x, y);
+              ctx.stroke();
+            }
+            preX = x;
+            preY = y;
+          }
+          ctx.restore();
+        };
+
+        Bloom.prototype.updateParams = function (): void {
+          for (let i = 1; i < this.joints.length; i++) {
+            if (i % 2 === 0) {
+              this.joints[i] += 0.005;
+            } else {
+              this.joints[i] -= 0.005;
+            }
+          }
+        };
+
+        Bloom.prototype.render = function (): void {
+          this.updateParams();
+          this.draw();
+        };
+
+        for (let i = 0; i < bloomNum; i++) {
+          const bloom = new Bloom(ctx, X / 2, Y / 2, i);
+          blooms.push(bloom);
+        }
+
+        /********************
+          Render
+        ********************/
+
+        function render(): void {
+          ctx.clearRect(0, 0, X, Y);
+          for (let i = 0; i < blooms.length; i++) {
+            blooms[i].render(i);
+          }
+          requestAnimationFrame(render);
+        }
+
+        render();
+
+        /********************
+          Event
+        ********************/
+
+        function onResize(): void {
+          X = canvas.current.width = window.innerWidth;
+          Y = canvas.current.height = window.innerHeight;
+          if (X < 768) {
+            minRadius = 70;
+            maxRadius = 100;
+          } else {
+            minRadius = 100;
+            maxRadius = 130;
+          }
+          blooms = [];
+          for (let i = 0; i < bloomNum; i++) {
+            const bloom = new Bloom(ctx, X / 2, Y / 2, i);
+            blooms.push(bloom);
           }
         }
-      }
-    };
 
-    while (balls.length < 25) {
-      const size = random(10, 20);
-      const ball = new Ball(
-        random(0 + size, width - size),
-        random(0 + size, height - size),
-        random(-7, 7),
-        random(-7, 7),
-        "rgb(" +
-          random(0, 255) +
-          "," +
-          random(0, 255) +
-          "," +
-          random(0, 255) +
-          ")",
-        size
-      );
-      balls.push(ball);
-    }
-    function loop(): void {
-      ctx.fillStyle = "rgba(0,0,0,0.25)";
-      ctx.fillRect(0, 0, width, height);
+        window.addEventListener("resize", function () {
+          onResize();
+        });
 
-      for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
-        balls[i].collisionDetect();
-      }
-      requestAnimationFrame(loop);
-    }
+        canvas.current.addEventListener("mousemove", function (e) {
+          mouseX = e.clientX;
+          mouseY = e.clientY;
+        });
 
-    loop();
+        canvas.current.addEventListener("wheel", function (e) {
+          const scrollY = e.deltaY;
+          if (scrollY < 0) {
+            blooms.pop();
+          }
+          if (scrollY > 0) {
+            const bloom = new Bloom(ctx, X / 2, Y / 2, 100);
+            blooms.push(bloom);
+          }
+        });
+
+        let touchStartY;
+        let touchMoveY;
+        let touchEndY;
+
+        canvas.current.addEventListener(
+          "touchstart",
+          function (e) {
+            const touch = e.targetTouches[0];
+            touchStartY = touch.pageY;
+          },
+          false
+        );
+        canvas.current.addEventListener(
+          "touchmove",
+          function (e) {
+            const touch = e.targetTouches[0];
+            touchMoveY = touch.pageY;
+            touchEndY = touchStartY - touchMoveY;
+            if (touchEndY > 0) {
+              for (let i = 0; i < Math.abs(touchEndY / 1000); i++) {
+                const bloom = new Bloom(ctx, X / 2, Y / 2, i);
+                blooms.push(bloom);
+              }
+            }
+            if (touchEndY < 0) {
+              for (let i = 0; i < Math.abs(touchEndY / 1000); i++) {
+                blooms.pop();
+              }
+            }
+          },
+          false
+        );
+        canvas.current.addEventListener(
+          "touchend",
+          function (e) {
+            touchStartY = null;
+            touchMoveY = null;
+            touchEndY = null;
+          },
+          false
+        );
+      });
+    })();
   }, []);
   return (
     <div>
