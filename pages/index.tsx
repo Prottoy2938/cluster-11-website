abf859f4efd8e0deb5289b8546dad5a0e0c135e1
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-16 10:50:58 +0600

    Basic steps is complete - manged to diplay the canvas on the background - browsing and testing other canvas design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import SearchBar from "../src/components/search-bar/search-bar";
+import BouncingBallCanvas from "../src/components/bouncing-ball-canvas/bouncing-ball-canvas";
 
 const Home: React.FC = () => {
-  return <SearchBar />;
+  return (
+    <>
+      <BouncingBallCanvas />
+      <SearchBar />
+    </>
+  );
 };
 
 export default Home;
--- src/components/bouncing-ball-canvas/bouncing-ball-canvas.tsx
+++ src/components/bouncing-ball-canvas/bouncing-ball-canvas.tsx
@@ -1,110 +1,112 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
+import styles from "./bouncing-ball-canvas.module.css";
 
-const BouncingBallCanvas = () => {
+const BouncingBallCanvas: React.FC = (props) => {
   const canvas = useRef(null);
 
-  const ctx = canvas.getContext("2d");
+  useEffect(() => {
+    const ctx = canvas.current.getContext("2d");
 
-  const width = (canvas.width = window.innerWidth);
-  const height = (canvas.height = window.innerHeight);
-  const balls = [];
+    const width = (canvas.current.width = window.innerWidth);
+    const height = (canvas.current.height = window.innerHeight);
+    const balls = [];
 
-  function random(min: number, max: number): number {
-    const num = Math.floor(Math.random() * (max - min)) + min;
-    return num;
-  }
-
-  function Ball(x, y, velX, velY, color, size): void {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-  }
-
-  Ball.prototype.draw = function (): void {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  };
-
-  Ball.prototype.update = function (): void {
-    if (this.x + this.size >= width) {
-      this.velX = -this.velX;
+    function random(min: number, max: number): number {
+      const num = Math.floor(Math.random() * (max - min)) + min;
+      return num;
     }
 
-    if (this.x - this.size <= 0) {
-      this.velX = -this.velX;
+    function Ball(x, y, velX, velY, color, size): void {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
     }
 
-    if (this.y + this.size >= height) {
-      this.velY = -this.velY;
-    }
-    if (this.y - this.size <= 0) {
-      this.velY = -this.velY;
-    }
-    this.x += this.velX;
-    this.y += this.velY;
-  };
+    Ball.prototype.draw = function (): void {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+    };
 
-  Ball.prototype.collisionDetect = function (): void {
-    for (const j = 0; j < balls.length; j++) {
-      if (!(this === balls[j])) {
-        const dx = this.x - balls[j].x;
-        const dy = this.y - balls[j].y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+    Ball.prototype.update = function (): void {
+      if (this.x + this.size >= width) {
+        this.velX = -this.velX;
+      }
 
-        if (distance < this.size + balls[j].size) {
-          balls[j].color = this.color =
-            "rgb(" +
-            random(0, 255) +
-            "," +
-            random(0, 255) +
-            "," +
-            random(0, 255) +
-            ")";
+      if (this.x - this.size <= 0) {
+        this.velX = -this.velX;
+      }
+
+      if (this.y + this.size >= height) {
+        this.velY = -this.velY;
+      }
+      if (this.y - this.size <= 0) {
+        this.velY = -this.velY;
+      }
+      this.x += this.velX;
+      this.y += this.velY;
+    };
+
+    Ball.prototype.collisionDetect = function (): void {
+      for (let j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+          const dx = this.x - balls[j].x;
+          const dy = this.y - balls[j].y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+
+          if (distance < this.size + balls[j].size) {
+            balls[j].color = this.color =
+              "rgb(" +
+              random(0, 255) +
+              "," +
+              random(0, 255) +
+              "," +
+              random(0, 255) +
+              ")";
+          }
         }
       }
+    };
+
+    while (balls.length < 25) {
+      const size = random(10, 20);
+      const ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
+        "rgb(" +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          ")",
+        size
+      );
+      balls.push(ball);
     }
-  };
+    function loop(): void {
+      ctx.fillStyle = "rgba(0,0,0,0.25)";
+      ctx.fillRect(0, 0, width, height);
 
-  while (balls.length < 25) {
-    const size = random(10, 20);
-    const ball = new Ball(
-      random(0 + size, width - size),
-      random(0 + size, height - size),
-      random(-7, 7),
-      random(-7, 7),
-      "rgb(" +
-        random(0, 255) +
-        "," +
-        random(0, 255) +
-        "," +
-        random(0, 255) +
-        ")",
-      size
-    );
-    balls.push(ball);
-  }
-  function loop(): void {
-    ctx.fillStyle = "rgba(0,0,0,0.25)";
-    ctx.fillRect(0, 0, width, height);
-
-    for (let i = 0; i < balls.length; i++) {
-      balls[i].draw();
-      balls[i].update();
-      balls[i].collisionDetect();
+      for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+      }
+      requestAnimationFrame(loop);
     }
-    requestAnimationFrame(loop);
-  }
 
-  loop();
-
+    loop();
+  }, []);
   return (
     <div>
-      <canvas ref={canvas}></canvas>
+      <canvas ref={canvas} className={styles.canvas}></canvas>
     </div>
   );
 };
